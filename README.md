990c00ae2cd1b95d7501e38a2ec3d1101efec734
SACHIN SHARMA <104039544+98sachin@users.noreply.github.com>
2023-12-14 08:47:20 +0000

    container_TaskyApp

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 123456-02
+
+    <!-- DOM (Document Object Model)
+        web development is all about dom manupulation.
+        
+    -->
---
+++ index.js
@@ -0,0 +1,303 @@
+// console.log("hello world!");
+
+// DYNAMIC APPROACH BELOW
+// var state = {
+//     taskList:[
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//          {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//          {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         }
+//     ]
+// }
+
+var state = {
+    taskList:[
+
+    ]
+};
+
+// DOM Objects
+
+var taskContents = document.querySelector(".task__contents");
+var taskModel = document.querySelector(".task__modal__body");
+
+// console.log(taskContents);
+
+// dynamic card creations
+var htmlTaskContent = ({id, title, description, type, url}) => // we use backticks(``) to write html code in javascript file. similarly to use javascript in html use "${...}".
+    `
+    <div class="col-md-6 col-lg-4 mt-3" id="${id}" key="${id}">
+      <div class="card shadow-sm task__card">
+        <div class="card-header d-flex justify-content-end task__card__header">
+          <button type="button" class="btn btn-outline-info mr-2" name="${id}" onclick="editTask.apply(this.arguments)">
+            <i class="fas fa-pencil-alt" name="${id}"></i>
+          </button>
+          <button
+            type="button"
+            class="btn btn-outline-danger mr-2"
+            name="${id}"
+            onclick="deleteTask.apply(this,arguments)"
+          >
+            <i class="fas fa-trash-alt" name="${id}"></i>
+          </button>
+        </div>
+        <div class="card-body">
+          ${ url ? `<img
+            width="100%"
+            src="${url}"
+            alt="card img cap"
+            class="card-img-top md-3 rounded-lg"
+          />` : `<img
+            width="100%"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXp7vG6vsG3u77s8fTCxsnn7O/f5OfFyczP09bM0dO8wMPk6ezY3eDd4uXR1tnJzdBvAX/cAAACVElEQVR4nO3b23KDIBRA0ShGU0n0//+2KmO94gWZ8Zxmr7fmwWEHJsJUHw8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwO1MHHdn+L3rIoK6eshsNJ8kTaJI07fERPOO1Nc1vgQm2oiBTWJ+d8+CqV1heplLzMRNonED+4mg7L6p591FC+133/xCRNCtd3nL9BlxWP++MOaXFdEXFjZ7r8D9l45C8y6aG0cWtP/SUGhs2d8dA/ZfGgrzYX+TVqcTNRRO9l+fS5eSYzQs85psUcuzk6igcLoHPz2J8gvzWaH/JLS+95RfOD8o1p5CU5R7l5LkfKEp0mQ1UX7hsVXqDpRrifILD/3S9CfmlUQFhQfuFu0STTyJ8gsP3PH7GVxN1FC4t2sbBy4TNRTu7LyHJbqaqKFw+/Q0ncFloo7CjRPwMnCWqKXQZ75El4nKC9dmcJaou9AXOE5UXbi+RGeJygrz8Uf+GewSn9uXuplnWDZJ7d8f24F/s6iq0LYf9olbS3Q8i5oKrRu4S9ybwaQ/aCkqtP3I28QDgeoK7TBya/aXqL5COx67PTCD2grtdOwH+pQV2r0a7YVBgZoKwwIVFQYG6ikMDVRTGByopjD8ATcKb0UhhRTe77sKs2DV7FKSjId18TUEBYVyLhUThWfILHTDqmI85/2RWWjcE/bhP6OD7maT3h20MHsA47JC3PsW0wcwLhv9t0OOPOIkCn21y2bXXwlyylxiYMPk1SuCSmpfK8bNQvIrpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwNX4BCbAju9/X67UAAAAASUVORK5CYII="
+            alt="card img cap"
+            class="card-img-top md-3 rounded-lg"
+          />` }
+          <h4 class="card-title">${title}</h4>
+          <p
+            class="description trim-3-lines text-muted data-gram_editor='false'"
+          >
+            ${description}
+          </p>
+          <div class="tags text-white d-flex flex-wrap">
+            <span class="badge bg-primary m-1">${type}</span>
+          </div>
+        </div>
+        <div class="card-footer">
+          <button
+            class="btn btn-outline-primary float-right"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#showTask"
+            id=${id}
+            onclick='openTask.apply(this,arguments)'
+          >
+            Open Task
+          </button>
+        </div>
+      </div>
+    </div>
+
+    `;
+
+
+// dynamic opentask modal contents
+var htmlModalContent = ({id, title, description,url})=>{
+  var date=new Date(parseInt(id));
+  return `
+  <div id=${id} >
+      ${ url ? `<img
+            width="100%"
+            src="${url}"
+            alt="card img cap"
+            class="card-img-top md-3 rounded-lg"
+          />` :
+        `<img
+            width="100%"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAMFBMVEXp7vG6vsG3u77s8fTCxsnn7O/f5OfFyczP09bM0dO8wMPk6ezY3eDd4uXR1tnJzdBvAX/cAAACVElEQVR4nO3b23KDIBRA0ShGU0n0//+2KmO94gWZ8Zxmr7fmwWEHJsJUHw8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAwO1MHHdn+L3rIoK6eshsNJ8kTaJI07fERPOO1Nc1vgQm2oiBTWJ+d8+CqV1heplLzMRNonED+4mg7L6p591FC+133/xCRNCtd3nL9BlxWP++MOaXFdEXFjZ7r8D9l45C8y6aG0cWtP/SUGhs2d8dA/ZfGgrzYX+TVqcTNRRO9l+fS5eSYzQs85psUcuzk6igcLoHPz2J8gvzWaH/JLS+95RfOD8o1p5CU5R7l5LkfKEp0mQ1UX7hsVXqDpRrifILD/3S9CfmlUQFhQfuFu0STTyJ8gsP3PH7GVxN1FC4t2sbBy4TNRTu7LyHJbqaqKFw+/Q0ncFloo7CjRPwMnCWqKXQZ75El4nKC9dmcJaou9AXOE5UXbi+RGeJygrz8Uf+GewSn9uXuplnWDZJ7d8f24F/s6iq0LYf9olbS3Q8i5oKrRu4S9ybwaQ/aCkqtP3I28QDgeoK7TBya/aXqL5COx67PTCD2grtdOwH+pQV2r0a7YVBgZoKwwIVFQYG6ikMDVRTGByopjD8ATcKb0UhhRTe77sKs2DV7FKSjId18TUEBYVyLhUThWfILHTDqmI85/2RWWjcE/bhP6OD7maT3h20MHsA47JC3PsW0wcwLhv9t0OOPOIkCn21y2bXXwlyylxiYMPk1SuCSmpfK8bNQvIrpAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADwNX4BCbAju9/X67UAAAAASUVORK5CYII="
+            alt="card img cap"
+            class="card-img-top md-3 rounded-lg"
+          />` }
+      <strong class='text-sm text-muted' >Created on ${date.toDateString()}</strong>
+      <h2 class='my-3' >${title}</h2>
+      <p class='lead' >${description}</p>
+  </div>
+  `
+}  
+
+// func is called whenever we need to update browser's local storage from the taskList array
+var updateLocalStorage = () => {
+  localStorage.setItem('task', JSON.stringify({
+    tasks: state.taskList,
+  }))
+}
+
+var loadInitialData = () => {
+  var localStorageCopy = JSON.parse(localStorage.tasks)
+
+  if(localStorageCopy) state.taskList = localStorageCopy.tasks;
+
+  state.taskList.map((cardDate)=>{
+    taskContents.insertAdjacentHTML("beforeend",htmlTaskContent(cardDate));
+  });
+};
+
+var handleSubmit = (event) => {
+  const id = `${Date.now()}`;
+  const input = {
+    url:document.getElementById('imageURL').value,
+    title:document.getElementById('taskTitle').value,
+    type:document.getElementById('tags').value,
+    description:document.getElementById('taskDescription').value,
+  };
+
+  if(input.title==='' || input.type==='' || input.description===''){
+    return alert("Please fill all the required fields");
+  }
+
+// remember Spread Operator.
+  taskContents.insertAdjacentHTML("beforeend",htmlTaskContent({
+    ...input, id
+  }))
+
+state.taskList.push({...input,id});
+updateLocalStorage();
+}
+
+var openTask = (e)=>{
+  if (!e) e= window.event ;
+
+  var getTask = state.taskList.find(({id})=>id===e.target.id);
+  taskModel.innerHTML = htmlModalContent(getTask);
+}
+
+var deleteTask = (e)=>{
+  if(!e) e = window.event;
+
+  var targetID = e.target.getAttribute("name");
+  var type = e.target.tagName;
+  // console.log(type);
+  var removeTask = state.taskList.filter(({id})=>id !== targetID);
+  // console.log(removeTask);  
+
+  state.taskList = removeTask;
+  updateLocalStorage();
+
+  if(type==="BUTTON"){
+    console.log(e.target.parentNode.parentNode.parentNode);
+    return e.target.parentNode.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode.parentNode
+    )
+  }
+   return e.target.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+      e.target.parentNode.parentNode.parentNode.parentNode
+    )
+};
+
+var editTask = (e)=>{
+  if(!e) e = window.event;
+  var targetID = e.target.id;
+  var type = e.target.tagName;
+
+  var parentNode;
+  let taskTitle;
+  var taskDescription;
+  var taskType;
+  let submitButton;
+
+  if (type==="BUTTON"){
+    parentNode = e.target.parentNode.parentNode
+  }else{
+    parentNode = e.target.parentNode.parentNode.parentNode
+  }
+  
+  taskTitle=parentNode.childNodes[3].childNodes[3];
+  // console.log(taskTitle);
+  taskDescription=parentNode.childNodes[3].childNodes[5];
+  taskType=parentNode.childNodes[3].childNodes[7].childNodes[1];
+  console.log(taskTitle,taskDescription,taskType);
+  submitButton = parentNode.childNodes[5].childNodes[1];
+
+  taskTitle.setAttribute("contenteditable","true");
+  taskDescription.setAttribute("contenteditable","true");
+  taskType.setAttribute("contenteditable","true");
+
+  submitButton.setAttribute('onclick',"saveEdit.apply(this,arguments)");
+  submitButton.removeAttribute("data-bs-toggle");
+  submitButton.removeAttribute("data-bs-target");
+  submitButton.innerHTML = "Save Changes";
+};
+
+var saveEdit = (e)=>{
+  if(!e) e = window.event;
+
+  var targetID = e.target.id;
+  var parentNode = e.target.parentNode.parentNode;
+  // console.log(parentNode);
+
+  var taskTitle=parentNode.childNodes[3].childNodes[3];
+  var taskDescription=parentNode.childNodes[3].childNodes[5];
+  var taskType=parentNode.childNodes[3].childNodes[7].childNodes[1];
+  var submitButton=parentNode.childNodes[5].childNodes[1];
+
+  var updateData={
+    taskTitle:taskTitle.innerHTML,
+    taskDescription:taskDescription.innerHTML,
+    taskType:taskType.innerHTML
+  };
+
+  var stateCopy=state.taskList;
+    stateCopy=stateCopy.map((task)=>
+    task.id===targetID
+    ?{
+      id:task.id,
+      title:updateData.taskTitle,
+      description:updateData.taskDescription,
+      type:updateData.taskType,
+      url:task.url,
+    }
+  : task
+);
+
+state.taskList = stateCopy;
+updateLocalStorage();
+
+taskTitle.setAttribute("contenteditable","false");
+taskDescription.setAttribute("contenteditable","false");
+taskType.setAttribute("contenteditable","false");
+
+submitButton.setAttribute("onclick", "openTask.apply(this,arguments)");
+submitButton.setAttribute("data-bs-toggle","modal");
+submitButton.setAttribute("data-bs-target","#showTask");
+submitButton.innerHTML="Open Task";
+
+};
+
+// var searchTask = (e)=>{
+//   if (!e) e = window.event;
+
+//   while (taskContents.firstChild){
+//     taskContents.removeChild(taskContents.firstChild);
+//   }
+
+//   var resultData=state.taskList.filter(({title})=>{
+//     title.includes(e.target.value);
+//   });
+
+//   console.log(resultData);
+
+//   resultData.map((cardDate)=>{
+//     taskContents.insertAdjacentHTML("beforeend",htmlTaskContent(cardDate));
+//   })
+// }
+
+// search functionality
+
+const searchTask=(e)=>{
+  if(!e) e=window.event;
+
+  while(taskContents.firstChild){
+    taskContents.removeChild(taskContents.firstChild);
+  }
+
+  const resultData=state.taskList.filter(({title})=>title.includes(e.target.value));
+
+  console.log(resultData);
+  resultData.map((cardData)=>{
+    taskContents.insertAdjacentHTML("beforeend",htmlTaskContent(cardData))
+  });
+}
